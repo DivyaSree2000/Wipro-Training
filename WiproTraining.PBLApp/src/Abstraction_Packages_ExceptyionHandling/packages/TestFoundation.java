9816b88ade8993f2a091cd9014582e974e95b272
DivyaSree2000 <ponagatisekhardivyasree@gmail.com>
2020-09-06 23:30:46 +0530

    WiproTraining

---
+++ WiproTraining.PBLApp/src/Abstraction_Packages_ExceptyionHandling/packages/TestFoundation.java
@@ -0,0 +1,9 @@
+package Abstraction_Packages_ExceptyionHandling.packages;
+import Abstraction_Packages_ExceptyionHandling.packages.test.Foundation;
+public class TestFoundation {
+  public static void main(String[] args) {
+      Foundation fn = new Foundation();
+      fn.var4 = 4;
+      System.out.println(fn.var4);
+  }
+}
