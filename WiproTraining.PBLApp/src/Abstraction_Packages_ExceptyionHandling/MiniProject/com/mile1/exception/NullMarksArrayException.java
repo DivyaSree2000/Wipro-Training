ae1f4c135b7aa3fb8005bc09e7f8d9077bbc9b63
DivyaSree2000 <ponagatisekhardivyasree@gmail.com>
2020-09-06 23:43:51 +0530

    WiproTraining

---
+++ WiproTraining.PBLApp/src/Abstraction_Packages_ExceptyionHandling/MiniProject/com/mile1/exception/NullMarksArrayException.java
@@ -0,0 +1,7 @@
+package Abstraction_Packages_ExceptyionHandling.MiniProject.com.mile1.exception;
+public class NullMarksArrayException extends Exception {
+	@Override
+	public String toString() {
+		return "NullMarksArrayException occurred";
+	}
+}
---
+++ WiproTraining.PBLApp/src/Abstraction_Packages_ExceptyionHandling/MiniProject/com/mile1/exception/NullStudentException.java
@@ -0,0 +1,7 @@
+package Abstraction_Packages_ExceptyionHandling.MiniProject.com.mile1.exception;
+public class NullStudentException extends Exception {
+	@Override
+	public String toString() {
+		return "NullStudentException occurred";
+	}
+}
